8823649bf2e592071e0c0411a541174f1b0e1b32
Darky <62349193+skanim-sdw@users.noreply.github.com>
2021-05-23 21:07:14 +0300

    Update darkyExceptions.py

--- modules/darkyExceptions.py
+++ modules/darkyExceptions.py
@@ -14,6 +14,7 @@ def get_error(error_code=0):
 		"10": "This command was deactivated",
 		"11": "User is admin",
 		"12": "Id is DarkyBot",
+		"13": "Bot in testing mode right now!",
 		
 		"50": "Wrong association name",
 		"51": "Association for current command not found",

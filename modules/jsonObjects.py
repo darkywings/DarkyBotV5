5284aa39689a2e3a5e5a4da31caee1dea8aa8482
Darky <62349193+skanim-sdw@users.noreply.github.com>
2021-11-14 20:00:03 +0300

    Update jsonObjects.py

--- modules/jsonObjects.py
+++ modules/jsonObjects.py
@@ -1,17 +1,17 @@
-import json
+import ujson
 
 class json_objects:
 	
 	def write(object, path): #запись json-обьекта в файл
 		#object - obj, обьект который будет записываться в файл
 		with open(path, 'w') as jsonFile:
-			jsonFile.write(json.dumps(object, indent=4, ensure_ascii=False))
+			jsonFile.write(ujson.dumps(object, indent=4, ensure_ascii=False))
 			jsonFile.close()
 	
 	
 	def load(path): #загрузка json-обьекта из файла
 		#path - string, путь к файлу
 		with open(path, 'r') as jsonFile:
-			jsonContent = json.load(jsonFile)
+			jsonContent = ujson.load(jsonFile)
 			jsonFile.close()
 		return jsonContent
